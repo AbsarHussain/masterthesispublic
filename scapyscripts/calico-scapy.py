4ba0442433ab63f2b1b3f5a262fd4104cae4c80b
AbsarHussain <absar.hussain@careem.com>
2022-12-26 04:43:50 +0500

    calico scapy file

---
+++ scapyscripts/calico-scapy.py
@@ -0,0 +1,7 @@
+from scapy.all import * 
+A = '192.111.111.101' # spoofed source IP address 
+B = 'DST_IP_OF_FLASK_SCAPY_DEPLOYMENT' # destination IP address 
+C = 80 # source port 
+D = 7000 # destination port \
+spoofed_packet = IP(src=A, dst=B) / TCP( dport=D) / payload 
+send(spoofed_packet) 
